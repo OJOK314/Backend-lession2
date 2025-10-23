4051b4a428b82ee82af87400441b3f78472d0fc3
ojok314 <ojokalfred1996@gmail.com>
2025-10-23 04:41:31 -0700

    add frontend intergration in to intergration

---
+++ Frontend-intergration/index.js
@@ -0,0 +1,10 @@
+let todobox = document.getElementById("todo-box")
+let apiurl = " https://backend-lession2-6.onrender.com"
+
+async function fetchData() {
+    let respones = await fetch(apiurl)
+    let data = await respones.json()
+    console.log(data)
+    
+}
+fetchData
