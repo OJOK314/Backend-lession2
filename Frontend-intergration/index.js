19b21c64cc614c63792bcfc21277f75916722d96
ojok314 <ojokalfred1996@gmail.com>
2025-10-24 03:59:21 -0700

    add some small changes in the intergration file

--- Frontend-intergration/index.js
+++ Frontend-intergration/index.js
@@ -1,12 +1,38 @@
+
+
 let todobox = document.getElementById('todo-box')
-let apiurl = 'https://backend-lession2-6.onrender.com/data'
+let apiUrl = 'https://backend-lession2-6.onrender.com/data'
 
 async function fetchData() {
-    let respones = await fetch(apiurl)
-    let data = await respones.json()
-    console.log(data)
+    let response = await fetch(apiUrl)
+    let data = await response.json()
+    return data
     
 }
 fetchData()
 
 
+// create a funtion that creates new lists element
+function createListItem(item) {
+
+    let newItem = document.createElement('li')
+
+    newItem.innerText = item.title
+    return newItem;
+    
+}
+// create a function to generate the list item from the data provided
+ async function generateList() {
+    let taskData = await fetchData()
+   
+
+    let todos = taskData.map((task) =>{
+    return createListItem(task)
+    })
+
+    console.log(todos)
+    todobox.append(...todos)
+     
+}
+
+generateList()
